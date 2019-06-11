6de69e75ea3174c633d29cf6f30f6c6e3521a83e
Thanh Pham <ThanhPham@Thanhs-MacBook-Air.local>
2019-06-11 15:08:11 +1000

    g

--- SoccerJahva.js
+++ SoccerJahva.js
@@ -2,17 +2,7 @@ document.getElementById("addteamsbtn").addEventListener("click", addteamsfunctio
 
 function addteamsFunction(){
 
-var teamarray = [];
-var teamname = document.getElementById("teamname").value
-var outputtone = document.getElementById("outputteamone").value
 
-if(teamname === ""){
-    alert("Please enter a team name")
-
-}else{
-    teamarray.push(teamname)
-document.getElementById("outputteamone").value = teamarray[0]
-}
 }
 
 document.getElementById("addteambutton").addEventListener("click", addteamsfunction);
