add6e5601211827562bb9751d24ab6d71d9b4d89
Thanh Pham <ThanhPham@Thanhs-MacBook-Air.local>
2019-08-13 15:19:41 +1000

    good

--- SoccerJahva.js
+++ SoccerJahva.js
@@ -1,70 +1,171 @@
-document.getElementById("addteamsbtn").addEventListener("click", addteamsfunction);
+document.getElementById("addteamsbtn").addEventListener("click", addteamsFunction);
+
+
+
 
 function addteamsFunction(){
 var teamsarray = []
+var seedsarray = []
 
-if(enterteamname1 == ""){
+
+if(enterteamname1.value == ""){
   alert("enter a team name for all teams")
   return
 }else{
-  teamsarray.push(enterteamname1)
+  teamsarray.push(enterteamname1.value)
 }
 
-if(enterteamname2 == ""){
+if(enterteamname2.value == ""){
   alert("enter a team name for all teams")
   return
 }else{
-  teamsarray.push(enterteamname2)
+  teamsarray.push(enterteamname2.value)
 }
 
-if(enterteamname3 == ""){
+if(enterteamname3.value == ""){
   alert("enter a team name for all teams")
   return
 }else{
-  teamsarray.push(enterteamname3)
+  teamsarray.push(enterteamname3.value)
 }
 
 
-if(enterteamname4 == ""){
+if(enterteamname4.value == ""){
   alert("enter a team name for all teams")
   return
 }else{
-  teamsarray.push(enterteamname4)
+  teamsarray.push(enterteamname4.value)
 }
 
-if(enterteamname5 == ""){
+if(enterteamname5.value == ""){
   alert("enter a team name for all teams")
   return
 }else{
-  teamsarray.push(enterteamname5)
+  teamsarray.push(enterteamname5.value)
 }
 
-if(enterteamname6 == ""){
+if(enterteamname6.value == ""){
   alert("enter a team name for all teams")
   return
 }else{
-  teamsarray.push(enterteamname6)
+  teamsarray.push(enterteamname6.value)
 }
 
 
-if(enterteamname7 == ""){
+if(enterteamname7.value == ""){
   alert("enter a team name for all teams")
   return
 }else{
-  teamsarray.push(enterteamname7)
+  teamsarray.push(enterteamname7.value)
 }
 
-if(enterteamname8 == ""){
+if(enterteamname8.value == ""){
   alert("enter a team name for all teams")
   return
 }else{
-  teamsarray.push(enterteamname8)
+  teamsarray.push(enterteamname8.value)
 }
+
+
+
+
+
+if(seedteam1.value == ""){
+  alert("enter a seed for all teams")
+  return
+}else{
+  seedsarray.push(seedteam1.value)
+}
+
+if(seedteam2.value == ""){
+  alert("enter a seed for all teams")
+  return
+}else{
+  seedsarray.push(seedteam2.value)
+}
+
+if(seedteam3.value == ""){
+  alert("enter a seed for all teams")
+  return
+}else{
+  seedsarray.push(seedteam3.value)
+}
+
+
+if(seedteam4.value == ""){
+  alert("enter a seed for all teams")
+  return
+}else{
+  seedsarray.push(seedteam4.value)
+}
+
+if(seedteam5.value == ""){
+  alert("enter a seed for all teams")
+  return
+}else{
+  seedsarray.push(seedteam5.value)
+}
+
+if(seedteam6.value == ""){
+  alert("enter a seed for all teams")
+  return
+}else{
+  seedsarray.push(seedteam6.value)
+}
+
+
+if(seedteam7.value == ""){
+  alert("enter a seed for all teams")
+  return
+}else{
+  seedsarray.push(seedteam7.value)
+}
+
+if(seedteam8.value == ""){
+  alert("enter a seed for all teams")
+  return
+}else{
+  seedsarray.push(seedteam8.value)
 }
 
 
 
-document.getElementById("addteambutton").addEventListener("click", addteamsfunction);
+outputteamone.value = teamsarray[0]
+outputteamtwo.value = teamsarray[1]
+outputteamthree.value = teamsarray[2]
+outputteamfour.value = teamsarray[3]
+outputteamfive.value = teamsarray[4]
+outputteamsix.value = teamsarray[5]
+outputteamseven.value = teamsarray[6]
+outputteameight.value = teamsarray[7]
+
+outputseedteam1.value = seedsarray[0]
+outputseedteam2.value = seedsarray[1]
+outputseedteam3.value = seedsarray[2]
+outputseedteam4.value = seedsarray[3]
+outputseedteam5.value = seedsarray[4]
+outputseedteam6.value = seedsarray[5]
+outputseedteam7.value = seedsarray[6]
+outputseedteam8.value = seedsarray[7]
+
+}
+
+
+
+document.getElementById("play").addEventListener("click", playFunction);
+
+function playFunction(teamsarray, seedsarray){
+
+
+
+
+
+}
+
+
+
+
+
 
 
 
